58e83d894124a953bed92955a019042a155853e2
Deep Bhimani <129931628+deepbhimani6@users.noreply.github.com>
2024-04-29 12:38:56 +0000

    Pagination with js

---
+++ pagination-js/main.js
@@ -0,0 +1,84 @@
+const cardContainer = document.querySelector(".card-container");
+const cards = Array.from(document.getElementsByClassName("card"));
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const pageNumbers = document.getElementById("page-numbers");
+
+const cardsPerPage = 4;
+let currentPage = 1;
+let totalPage = Math.ceil(cards.length / cardsPerPage);
+
+for (let i = 0; i < totalPage; i++) {
+  const createElement = document.createElement("a");
+  createElement.setAttribute("href", "javascript:void(0);");
+  createElement.classList.add("page-link");
+  if (i == 0) {
+    createElement.classList.add("active");
+  }
+  createElement.textContent = i + 1;
+  document.getElementById("page-counters").appendChild(createElement);
+}
+
+let pageLinks = document.querySelectorAll(".page-link");
+
+let cardContainerWidth = cardContainer.getBoundingClientRect().width;
+
+function changePage() {
+  displayPage = currentPage - 1;
+  cardContainer.style.transform =
+    "translateX(calc(-" + cardContainerWidth + "px * " + displayPage + "))";
+}
+changePage();
+
+window.addEventListener(
+  "resize",
+  function (e) {
+    e.preventDefault();
+    cardContainerWidth = cardContainer.getBoundingClientRect().width;
+    changePage();
+  },
+  true
+);
+
+function updatePagination() {
+  pageNumbers.textContent = `page ${currentPage} of ${totalPage}`;
+  currentPage == 1
+    ? prevButton.setAttribute("disabled", "")
+    : prevButton.removeAttribute("disabled");
+  currentPage == totalPage
+    ? nextButton.setAttribute("disabled", "")
+    : nextButton.removeAttribute("disabled");
+  pageLinks.forEach((pageLink) => {
+    const page = parseInt(pageLink.textContent);
+    if (page == currentPage) pageLink.classList.add("active");
+    else pageLink.classList.remove("active");
+  });
+}
+updatePagination();
+
+prevButton.addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    // displayPage();
+    changePage();
+    updatePagination();
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (currentPage < totalPage) {
+    currentPage++;
+    // displayPage();
+    changePage();
+    updatePagination();
+  }
+});
+
+pageLinks.forEach((pageLink) => {
+  pageLink.addEventListener("click", () => {
+    currentPage = pageLink.textContent;
+    // displayPage();
+    changePage();
+    updatePagination();
+  });
+});
